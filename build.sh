36dccf8cbd5307381793107c7734dcb1a5b14c85
Thomas Chaplin <tomrchaplin@gmail.com>
2023-04-11 11:08:05 +0100

    Add wasm-pack install to build script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 toolchain=$(cat ./rust-toolchain)
 rustup toolchain install $toolchain
 rustup component add rust-src --toolchain $toolchain
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 # Build the project for release
 wasm-pack build --release --target web
 cd www
